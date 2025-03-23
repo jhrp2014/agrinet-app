c83ead5e19b74b51d27c6dac41cf6ab4d430e544
jhrp2014 <jhrpalmeira@gmail.com>
2025-03-23 01:56:28 -0400

    Commit inicial do projeto AgriNet

---
+++ agrinet-project/src/AgriNetApp.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { Search, Calendar, MessageCircle, Bell, UserPlus, BarChart2, Briefcase, Users, Menu, X, Mail, Lock, User, ArrowRight, Eye, EyeOff } from 'lucide-react';
+
+// ... (CÓDIGO OCULTADO POR LIMITAÇÃO - DEVE SER INSERIDO COMPLETAMENTE AQUI EM USO REAL)
+
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AgriNetApp from './AgriNetApp'
+
+const App = () => {
+  return <AgriNetApp />
+}
+
+export default App
